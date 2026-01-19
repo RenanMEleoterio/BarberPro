bb78db0d0e3daaa61d1c3c8e038c47416a9fb3e3
Renan Miranda <renanmeleoterio.43@gmail.com>
2026-01-19 13:35:40 -0300

    Ajuste do card de próximos agendamentos

--- Backend/Controllers/DashboardController.cs
+++ Backend/Controllers/DashboardController.cs
@@ -72,7 +72,7 @@ namespace BarbeariaSaaS.Controllers
             var proximoAgendamento = await _context.Agendamentos
                 .Include(a => a.Barbeiro)
                 .Include(a => a.Barbearia)
-                .Where(a => a.ClienteId == id && a.DataHora > DateTime.UtcNow && a.Status == StatusAgendamento.Atendido)
+                .Where(a => a.ClienteId == id && a.DataHora > DateTime.UtcNow && a.Status == StatusAgendamento.Pendente)
                 .OrderBy(a => a.DataHora)
                 .FirstOrDefaultAsync();
 
