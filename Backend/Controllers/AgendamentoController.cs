2e0e67a82708dc80fd88a52fcae43b7e94976c11
Renan Miranda <renanmeleoterio.43@gmail.com>
2026-03-06 23:38:55 -0300

    fix: adiciona using ausente para ILogger no AgendamentoController

--- Backend/Controllers/AgendamentoController.cs
+++ Backend/Controllers/AgendamentoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Security.Claims;
 using BarbeariaSaaS.Data;
 using BarbeariaSaaS.DTOs;
